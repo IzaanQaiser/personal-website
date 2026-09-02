b72e668fe1608f69a5d3a980f9911dec9936a989
Izaan Qaiser <izaanqaiser05@gmail.com>
2026-09-02 02:26:29 -0400

    chore(site): update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,11 +6,7 @@ export const metadata: Metadata = {
   description: 'Created with v0',
   generator: 'v0.dev',
   icons: {
-    icon: [
-      { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
+    icon: '/favicon.png',
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
